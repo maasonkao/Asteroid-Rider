a7536910d64cf89c38312f649acacb5c3a3799a2
akyrimos <akyrimos@uw.edu>
2023-03-05 02:56:28 -0800

    Update TurnManager.cs

--- Asteroid Rider/Assets/Scripts/TurnManager.cs
+++ Asteroid Rider/Assets/Scripts/TurnManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
+
 
 public class TurnManager : MonoBehaviour
 {
+    public List<int> playersList;
+    public int currentTurn;
+    public int initialPlayers;
+    public int currentPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i=0; i<initialPlayers; i++)
+        {
+            playersList.Add(i);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void NextTurn()
+    {
+        currentTurn++;
+        if(currentTurn >= playersList.Count)
+        {
+            currentTurn = 0;
+        }
+        currentPlayer = playersList[currentTurn];
     }
 }
