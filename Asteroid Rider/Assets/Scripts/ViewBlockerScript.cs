fb33c43a92c1402f8f774d55125508b7dc1aaaa2
akyrimos <maasongmx@gmail.com>
2023-05-12 15:20:35 -0700

    viewblockertext now highlights on hover

--- Asteroid Rider/Assets/Scripts/ViewBlockerScript.cs
+++ Asteroid Rider/Assets/Scripts/ViewBlockerScript.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
 
 public class ViewBlockerScript : MonoBehaviour
 {
     public GameManager gameManager;
     [SerializeField] Canvas mainCanvas;
+    public TextMeshPro viewBlockerText;
 
-
-    private void Start()
-    {
-       // var gm = gameManager.GetComponent<GameManager>();    
-    }
     private void OnMouseDown()
     {
         gameManager.SetCamera();
         mainCanvas.enabled = true;
+    }
+
+    void OnMouseOver()
+    {
+        viewBlockerText.color = new Color32(0xD1, 0x91, 0x23, 0xFF);
+    }
+
+    void OnMouseExit()
+    {
+        viewBlockerText.color = Color.white;
+
     }
 }
